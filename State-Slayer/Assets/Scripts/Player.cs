c83d43f6880bdb398ede85f90387eb7dc59ad528
hyunjunbaek <hjbaek5796@gmail.com>
2025-04-02 15:20:18 +0900

    Organize code w/ methods

--- State-Slayer/Assets/Scripts/Player.cs
+++ State-Slayer/Assets/Scripts/Player.cs
@@ -10,25 +10,46 @@ public class Player : MonoBehaviour
     public bool isMoving;
     private float xInput;
 
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
     }
 
+
     void Update()
+    {
+        Movement();
+        CheckInput();
+
+        AnimatorControllers();
+    }
+
+    private void CheckInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
 
-        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
-
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            Jump();
         }
+    }
 
+    private void Movement()
+    {
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+
+    private void AnimatorControllers()
+    {
         isMoving = rb.linearVelocityX != 0;
-
         anim.SetBool("isMoving", isMoving);
     }
 }
