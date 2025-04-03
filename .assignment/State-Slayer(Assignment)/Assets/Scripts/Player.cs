66ea51421888c271ca36065bcf1e4f61027a2445
hyunjunbaek <hjbaek5796@gmail.com>
2025-04-03 22:20:54 +0900

    Add jump/fall and attack animation

--- .assignment/State-Slayer(Assignment)/Assets/Scripts/Player.cs
+++ .assignment/State-Slayer(Assignment)/Assets/Scripts/Player.cs
@@ -7,11 +7,24 @@ public class Player : MonoBehaviour
     private Animator anim;
 
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Attack Info")]
+    [SerializeField] private float comboTime = 0.3f;
+    private float comboTimeWindow;
+    private bool isAttacking;
+    private int comboCounter;
 
     private float xInput;
 
     private int facingDir = 1;
     private bool facingRight = true;
+
+    [Header("Collision Info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+
 
     void Start()
     {
@@ -24,21 +37,89 @@ public class Player : MonoBehaviour
     {
         CheckInput();
         Movement();
+        CheckCollision();
+
+        comboTimeWindow -= Time.deltaTime;
 
         FlipController();
         AnimatorControllers();
     }
 
 
+    public void AttackOver()
+    {
+        isAttacking = false;
+
+        comboCounter++;
+
+        if (comboCounter > 2)
+            comboCounter = 0;
+    }
+
+
+    private void CheckCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+
     private void CheckInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartAttackEvent();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+
+    private void StartAttackEvent()
+    {
+        if (!isGrounded)
+            return;
+
+        if (comboTimeWindow < 0)
+            comboCounter = 0;
+
+        isAttacking = true;
+        comboTimeWindow = comboTime;
     }
 
 
     private void Movement()
     {
-        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        if (isAttacking)
+        {
+            rb.linearVelocity = new Vector2(0, 0);
+        }
+
+        else
+        {
+            rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        }
+    }
+
+
+    private void Jump()
+    {
+        if (isGrounded)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+
+    private void AnimatorControllers()
+    {
+        anim.SetFloat("yVelocity", rb.linearVelocityY);
+        anim.SetBool("isMoving", rb.linearVelocityX != 0);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isAttacking", isAttacking);
+        anim.SetInteger("comboCounter", comboCounter);
     }
 
 
@@ -58,11 +139,8 @@ public class Player : MonoBehaviour
     }
 
 
-    private void AnimatorControllers()
+    private void OnDrawGizmos()
     {
-        //bool isMoving = rb.linearVelocityX != 0;
-
-        anim.SetFloat("yVelocity", rb.linearVelocityY);
-        anim.SetBool("isMoving", rb.linearVelocityX != 0);
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
