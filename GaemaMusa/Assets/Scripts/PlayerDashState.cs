dd70dd99b81a8137b012243c17f0c34ba832b9af
hyunjunbaek <hjbaek5796@gmail.com>
2025-04-08 11:29:15 +0900

    Update transition between player dash and wallslide

--- GaemaMusa/Assets/Scripts/PlayerDashState.cs
+++ GaemaMusa/Assets/Scripts/PlayerDashState.cs
@@ -21,6 +21,9 @@ public class PlayerDashState : PlayerState
     {
         base.Update();
 
+        if (!player.IsGroundDetected() && player.IsWallDetected())
+            stateMachine.ChangeState(player.wallSlideState);
+
         player.SetVelocity(player.dashSpeed * player.dashDir, 0);
 
         if (stateTimer < 0)
