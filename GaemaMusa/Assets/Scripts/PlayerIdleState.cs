4e1f1744fac4164bd39287ad41c5fec37d2aa4e7
hyunjunbaek <hjbaek5796@gmail.com>
2025-04-08 10:50:56 +0900

    Update wallslide to only activate when player jumps on wall

--- GaemaMusa/Assets/Scripts/PlayerIdleState.cs
+++ GaemaMusa/Assets/Scripts/PlayerIdleState.cs
@@ -19,6 +19,9 @@ public class PlayerIdleState : PlayerGroundedState
     {
         base.Update();
 
+        if (xInput == player.facingDir && player.IsWallDetected())
+            return;
+
         if (xInput != 0)
             stateMachine.ChangeState(player.moveState);
     }
