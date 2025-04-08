0081103cb5370d666f6a45aa3fd9dfa9349cd435
hyunjunbaek <hjbaek5796@gmail.com>
2025-04-08 14:32:48 +0900

    Add player attack combo + animation

--- GaemaMusa/Assets/Scripts/PlayerPrimaryAttack.cs
+++ GaemaMusa/Assets/Scripts/PlayerPrimaryAttack.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class PlayerPrimaryAttack : PlayerState
 {
+    private int comboCounter;
+
+    private float lastTimeAttacked;
+    private float comboWindow = 2;
+
+
     public PlayerPrimaryAttack(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) 
         : base(_player, _stateMachine, _animBoolName)
     {
@@ -12,6 +18,11 @@ public class PlayerPrimaryAttack : PlayerState
     public override void Enter()
     {
         base.Enter();
+
+        if (comboCounter > 2 || Time.time >= lastTimeAttacked + comboWindow)
+            comboCounter = 0;
+
+        player.anim.SetInteger("ComboCounter", comboCounter);
     }
 
 
@@ -27,6 +38,9 @@ public class PlayerPrimaryAttack : PlayerState
     public override void Exit()
     {
         base.Exit();
+
+        comboCounter++;
+        lastTimeAttacked = Time.time;
     }
 
 }
