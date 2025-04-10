c34b1cde8a600b87772f550d81b6246fbec71db4
hyunjunbaek <hjbaek5796@gmail.com>
2025-04-10 11:01:28 +0900

    Fix skeleton battle state bug

--- GaemaMusa/Assets/Scripts/Enemy.cs
+++ GaemaMusa/Assets/Scripts/Enemy.cs
@@ -8,6 +8,9 @@ public class Enemy : Entity
     public float moveSpeed;
     public float idleTime;
 
+    [Header("Attack Info")]
+    public float attackDistance;
+
 
     public EnemyStateMachine stateMachine { get; private set; }
 
@@ -25,10 +28,17 @@ public class Enemy : Entity
         base.Update();
 
         stateMachine.currentState.Update();
-
-        Debug.Log(IsPlayerDetected().collider.gameObject.name + "Looking at Player");
     }
 
 
     public virtual RaycastHit2D IsPlayerDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right * facingDir, 50, whatIsPlayer);
+
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + attackDistance * facingDir, transform.position.y));
+    }
 }
