576a45acff54cfdee1b6e2af98ed819e57d8fc88
hyunjunbaek <hjbaek5796@gmail.com>
2025-04-11 11:55:03 +0900

    Debug

--- GaemaMusa/Assets/Scripts/Player/PlayerPrimaryAttackState.cs
+++ GaemaMusa/Assets/Scripts/Player/PlayerPrimaryAttackState.cs
@@ -18,6 +18,7 @@ public class PlayerPrimaryAttackState : PlayerState
     public override void Enter()
     {
         base.Enter();
+        xInput = 0;
 
         if (comboCounter > 2 || Time.time >= lastTimeAttacked + comboWindow)
             comboCounter = 0;
@@ -28,7 +29,10 @@ public class PlayerPrimaryAttackState : PlayerState
         float attackDir = player.facingDir;
 
         if (xInput != 0)
+        {
+            //Debug.Log(xInput);
             attackDir = xInput;
+        }
 
         player.SetVelocity(player.attackMovement[comboCounter].x * attackDir, player.attackMovement[comboCounter].y);
 
