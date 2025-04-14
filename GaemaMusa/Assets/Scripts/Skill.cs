24a3d26118cde976ed734ba30cb768f0ffc8af3d
hyunjunbaek <hjbaek5796@gmail.com>
2025-04-14 10:16:31 +0900

    Implement skill cooldown

---
+++ GaemaMusa/Assets/Scripts/Skill.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Skill : MonoBehaviour
+{
+    [SerializeField] protected float cooldown;
+    protected float cooldownTimer;
+
+
+    protected virtual void Update()
+    {
+        cooldownTimer -= Time.deltaTime;
+    }
+
+
+    public virtual bool CanUseSkill()
+    {
+        if (cooldownTimer < 0)
+        {
+            cooldownTimer = cooldown;
+            return true;
+        }
+
+        Debug.Log("Skill is on cooldown");
+
+        return false;
+    }
+}
