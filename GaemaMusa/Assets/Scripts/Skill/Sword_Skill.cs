ef2d722c0598aadd266813e16f5067df8d69d833
hyunjunbaek <hjbaek5796@gmail.com>
2025-04-16 13:57:02 +0900

    Update sword throw to aim w/ dots

--- GaemaMusa/Assets/Scripts/Skill/Sword_Skill.cs
+++ GaemaMusa/Assets/Scripts/Skill/Sword_Skill.cs
@@ -1,23 +1,50 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Sword_Skill : Skill
 {
     [Header("Skill Info")]
     [SerializeField] private GameObject swordPrefab;
-    [SerializeField] private Vector2 launchDir;
+    [SerializeField] private Vector2 launchForce;
     [SerializeField] private float swordGravity;
 
 
     private Vector2 finalDir;
 
 
+    [Header("Show Aim")]
+    [SerializeField] private int numberOfDots;
+    [SerializeField] private float spaceBetweenDots;
+    [SerializeField] private GameObject dotPrefab;
+    [SerializeField] private Transform dotsParent;
+
+    private GameObject[] dots;
+
+
+    private void Start()
+    {
+        base.Start();
+        GenerateDots();
+    }
+
+
     protected override void Update()
     {
         if (Input.GetKeyUp(KeyCode.Mouse1))
         {
-            finalDir = new Vector2(AimDirection().normalized.x * launchDir.x, AimDirection().normalized.y * launchDir.y);
-        }   
+            finalDir = new Vector2(AimDirection().normalized.x * launchForce.x, AimDirection().normalized.y * launchForce.y);
+        }
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            for (int i = 0; i < dots.Length; i++)
+            {
+                dots[i].transform.position = DotsPosition(i * spaceBetweenDots);
+            }
+        }
     }
+
+
 
 
     public void CreateSword()
@@ -26,6 +53,8 @@ public class Sword_Skill : Skill
         Sword_Skill_Controller newSwordScript = newSword.GetComponent<Sword_Skill_Controller>();
 
         newSwordScript.SetupSword(finalDir, swordGravity);
+
+        DotsActive(false);
     }
 
 
@@ -36,5 +65,36 @@ public class Sword_Skill : Skill
         Vector2 direction = mousePosition - playerPosition;
 
         return direction;
+    }
+
+
+    public void DotsActive(bool _isActive)
+    {
+        for (int i = 0; i < dots.Length; i++)
+        {
+            dots[i].SetActive(_isActive);
+        }
+    }
+
+
+    private void GenerateDots()
+    {
+        dots = new GameObject[numberOfDots];
+
+        for (int i = 0; i < numberOfDots; i++)
+        {
+            dots[i] = Instantiate(dotPrefab, player.transform.position, Quaternion.identity, dotsParent);
+            dots[i].SetActive(false);
+        }
+    }
+
+
+    private Vector2 DotsPosition(float t)
+    {
+        Vector2 position = (Vector2)player.transform.position + new Vector2(
+            AimDirection().normalized.x * launchForce.x,
+            AimDirection().normalized.y * launchForce.y) * t + 0.5f * (Physics2D.gravity * swordGravity) * (t * t);
+
+        return position;
     }
 }
