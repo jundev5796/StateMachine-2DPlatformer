ef5e3d488a023eeb4a6a332695925aa5aeea58ab
hyunjunbaek <hjbaek5796@gmail.com>
2025-04-16 13:27:58 +0900

    Update sword throw to aim with mouse

--- GaemaMusa/Assets/Scripts/Skill/Sword_Skill.cs
+++ GaemaMusa/Assets/Scripts/Skill/Sword_Skill.cs
@@ -8,11 +8,33 @@ public class Sword_Skill : Skill
     [SerializeField] private float swordGravity;
 
 
+    private Vector2 finalDir;
+
+
+    protected override void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            finalDir = new Vector2(AimDirection().normalized.x * launchDir.x, AimDirection().normalized.y * launchDir.y);
+        }   
+    }
+
+
     public void CreateSword()
     {
         GameObject newSword = Instantiate(swordPrefab, player.transform.transform.position, transform.rotation);
         Sword_Skill_Controller newSwordScript = newSword.GetComponent<Sword_Skill_Controller>();
 
-        newSwordScript.SetupSword(launchDir, swordGravity);
+        newSwordScript.SetupSword(finalDir, swordGravity);
+    }
+
+
+    public Vector2 AimDirection()
+    {
+        Vector2 playerPosition = player.transform.position;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // bring mouse position to world
+        Vector2 direction = mousePosition - playerPosition;
+
+        return direction;
     }
 }
