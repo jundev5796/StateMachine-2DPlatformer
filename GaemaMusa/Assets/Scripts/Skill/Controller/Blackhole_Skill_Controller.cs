c3136da3c694eb466c48d30cce2a4ff082cc8b03
hyunjunbaek <hjbaek5796@gmail.com>
2025-04-18 14:42:47 +0900

    Blackhole skill update

---
+++ GaemaMusa/Assets/Scripts/Skill/Controller/Blackhole_Skill_Controller.cs
@@ -0,0 +1,59 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blackhole_Skill_Controller : MonoBehaviour
+{
+    [SerializeField] private GameObject hotKeyPrefab;
+    [SerializeField] private List<KeyCode> keyCodeList;
+
+    public float maxSize;
+    public float growSpeed;
+    public bool canGrow;
+
+    private List<Transform> targets = new List<Transform>();
+
+
+    void Update()
+    {
+        if (canGrow)
+        {
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(maxSize, maxSize), growSpeed * Time.deltaTime);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Enemy>() != null)
+        {
+            collision.GetComponent<Enemy>().FreezeTime(true);
+
+            CreateHotKey(collision);
+        }
+    }
+
+
+    private void CreateHotKey(Collider2D collision)
+    {
+        if (keyCodeList.Count <= 0)
+        {
+            Debug.LogWarning("Check for Input");
+            return;
+        }
+
+        GameObject newHotKey = Instantiate(hotKeyPrefab, collision.transform.position + new Vector3(0, 2), Quaternion.identity);
+
+        KeyCode choosenKey = keyCodeList[Random.Range(0, keyCodeList.Count)];
+        keyCodeList.Remove(choosenKey);
+
+        Blackhole_HotKey_Controller newHotKeyScript = newHotKey.GetComponent<Blackhole_HotKey_Controller>();
+
+        newHotKeyScript.SetupHotKey(choosenKey, collision.transform, this);
+    }
+
+
+    public void AddEnemyToList(Transform _enemyTransform) => targets.Add(_enemyTransform);
+
+    //targets.Add(collision.transform);
+}
