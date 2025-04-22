b6a9791e8f5ada1379efc4e57608ff95fb92a806
hyunjunbaek <hjbaek5796@gmail.com>
2025-04-22 10:38:38 +0900

    Update crystal to explode + radius increase

--- GaemaMusa/Assets/Scripts/Skill/Controller/Crystal_Skill_Controller.cs
+++ GaemaMusa/Assets/Scripts/Skill/Controller/Crystal_Skill_Controller.cs
@@ -2,13 +2,24 @@ using UnityEngine;
 
 public class Crystal_Skill_Controller : MonoBehaviour
 {
+    private Animator anim => GetComponent<Animator>();
+    private CircleCollider2D cd => GetComponent<CircleCollider2D>();
     private float crystalExistTimer;
 
+    private bool canExplode;
+    private bool canMove;
+    private float moveSpeed;
 
-    public void SetupCrystal(float _crystalDuration)
+    private bool canGrow;
+    [SerializeField] private float growSpeed;
+
+
+    public void SetupCrystal(float _crystalDuration, bool _canExplode, bool _canMove, float _moveSpeed)
     {
         crystalExistTimer = _crystalDuration;
-
+        canExplode = _canExplode;
+        canMove = _canMove;
+        moveSpeed = _moveSpeed;
     }
 
 
@@ -18,8 +29,38 @@ public class Crystal_Skill_Controller : MonoBehaviour
 
         if (crystalExistTimer < 0)
         {
+            FinishCrystal();
+        }
+
+        if (canGrow)
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(3, 3), growSpeed * Time.deltaTime);
+    }
+
+
+    private void AnimationExplodeEvent()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, cd.radius);
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+                hit.GetComponent<Enemy>().Damage();
+        }
+    }
+
+
+    public void FinishCrystal()
+    {
+        if (canExplode)
+        {
+            canGrow = true;
+            anim.SetTrigger("Explode");
+        }
+
+        else
+        {
             SelfDestroy();
-        }   
+        }
     }
 
 
--- GaemaMusa/Assets/Scripts/Skill/Crystal_Skill.cs
+++ GaemaMusa/Assets/Scripts/Skill/Crystal_Skill.cs
@@ -6,6 +6,13 @@ public class Crystal_Skill : Skill
     [SerializeField] private GameObject crystalPrefab;
     private GameObject currentCrystal;
 
+    [Header("Explosive crystal")]
+    [SerializeField] private bool canExplode;
+
+    [Header("Moving crystal")]
+    [SerializeField] private bool canMoveToEnemy;
+    [SerializeField] private float moveSpeed;
+
 
     public override void UseSkill()
     {
@@ -16,13 +23,18 @@ public class Crystal_Skill : Skill
             currentCrystal = Instantiate(crystalPrefab, player.transform.position, Quaternion.identity);
             Crystal_Skill_Controller currentCrystalScript = currentCrystal.GetComponent<Crystal_Skill_Controller>();
             
-            currentCrystalScript.SetupCrystal(crystalDuration);
+            currentCrystalScript.SetupCrystal(crystalDuration, canExplode, canMoveToEnemy, moveSpeed);
         }
 
         else
         {
+            Vector2 playerPos = player.transform.position;
+
             player.transform.position = currentCrystal.transform.position;
-            Destroy(currentCrystal);
+
+
+            player.transform.position = currentCrystal.transform.position;
+            currentCrystal.GetComponent<Crystal_Skill_Controller>()?.FinishCrystal();
         }
     }
 }
