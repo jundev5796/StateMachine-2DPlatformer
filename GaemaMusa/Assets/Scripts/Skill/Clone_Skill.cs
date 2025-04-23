579941695cef1c3721a686dc01324859c36e1230
hyunjunbaek <hjbaek5796@gmail.com>
2025-04-23 09:33:58 +0900

    Refactor clone skill w/ coroutine

--- GaemaMusa/Assets/Scripts/Skill/Clone_Skill.cs
+++ GaemaMusa/Assets/Scripts/Skill/Clone_Skill.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Clone_Skill : Skill
@@ -38,6 +39,14 @@ public class Clone_Skill : Skill
     public void CreateCloneOnCounterAttack(Transform _enemyTransform)
     {
         if (canCreateCloneOnCounterAttack)
-            CreateClone(_enemyTransform, new Vector3(2 * player.facingDir, 0));
+            StartCoroutine(CreateCloneWithDelay(_enemyTransform, new Vector3(2 * player.facingDir, 0)));
+    }
+
+
+    private IEnumerator CreateCloneWithDelay(Transform _transform, Vector3 _offset)
+    {
+        yield return new WaitForSeconds(0.4f);
+
+        CreateClone(_transform, _offset);
     }
 }
